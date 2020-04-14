d8fbce6ca1fd62e3e02cad2600f03d7f33bf4b8e
chen.peng <42893501+pChenGithub@users.noreply.github.com>
2020-04-14 14:09:16 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,7 +1,7 @@
 
 #编译ffmpeg
 #交叉编译
-./configure --prefix=/home/cp/build_something/FFmpeg-release-4.1/_install_arm --arch=arm --target-os=linux --enable-cross-compile  --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+./configure --prefix=/home/cp/build_something/FFmpeg-release-4.1/_install_arm --enable-shared --arch=arm --target-os=linux --enable-cross-compile  --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 make 
 make install
 
