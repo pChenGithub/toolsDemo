7f674fbace99cce5e209d2555ce772589387ecde
chen.peng <42893501+pChenGithub@users.noreply.github.com>
2020-04-21 08:58:16 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -8,5 +8,7 @@ make install
 
 #编译libx264
 #交叉编译
-./configure --prefix=/home/cp/build_something/x264-master/_install_arm --disable-asm --enable-shared --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+./configure --prefix=/home/cp/build_something/x264-master/_install_arm --disable-asm --enable-shared --host=arm-linux --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+
+
 
