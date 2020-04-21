8efe07dc3ced2279c64ace72e13d8b16bae7aa20
chen.peng <42893501+pChenGithub@users.noreply.github.com>
2020-04-21 08:55:20 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,9 +1,12 @@
 
 #编译ffmpeg
 #交叉编译
-./configure --prefix=/home/cp/build_something/FFmpeg-release-4.1/_install_arm --enable-shared --arch=arm --target-os=linux --enable-cross-compile  --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+./configure --enable-libx264 --enable-gpl --prefix=/home/cp/build_something/FFmpeg-release-4.1/_install_arm --enable-shared --arch=arm --target-os=linux --enable-cross-compile  --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 make 
 make install
 
 
+#编译libx264
+#交叉编译
+./configure --prefix=/home/cp/build_something/x264-master/_install_arm --disable-asm --enable-shared --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
