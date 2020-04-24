7696ce06f31b667af417a614b2dbbb8ca2ef8913
chen.peng <42893501+pChenGithub@users.noreply.github.com>
2020-04-24 10:31:13 +0800

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -11,4 +11,36 @@ make install
 ./configure --prefix=/home/cp/build_something/x264-master/_install_arm --disable-asm --enable-shared --host=arm-linux --cross-prefix=/home/cp/gcc_tools/gcc-linaro-5.4.1-2017.05-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 
+#编译qt5.9.6
+#脚本文件
+
+#! /bin/sh
+PWD=$(pwd)
+
+echo "build qt source codec ..."
+echo "export gcc path ..."
+source /home/cp/gcc_tools/mkenv-gcc-5.4.1.sh
+echo "done"
+
+echo "path: ${PWD}"
+
+echo "config ..."
+./configure -prefix ${PWD}/_install_arm \
+    -xplatform arm-linux-gnueabihf-gcc \
+    -nomake examples \
+    -nomake tests    \
+    -nomake tools \
+    -opensource \
+    -confirm-license \
+    -no-opengl
+#    -opengl es2 \
+#    -gstreamer \
+
+#./configure -prefix ${PWD}/_install
+echo "done"
+echo "done"
+
+
+
+
 
